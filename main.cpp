f3f9e1d343b2f7dccbaad24c0525e18d0ec6e282
munenemaryann56 <113587381+munenemaryann56@users.noreply.github.com>
2022-11-10 05:29:39 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,35 +1,23 @@
 #include <iostream>
-//program to display 3 by 3 matrix
 
 using namespace std;
 
-int main()
-{
-    int a[3][3],i,j;
-    for(i=0;i<3;i++)
-    {
-
-    for(j=0;j<3;j++)
-
-   {
-
-    cout<<"enter number in pocket["<<i<<"]["<<j<<"]";
-    cin>>a[i][j];
-   }
-    }
-
-    cout<<"mstrix is  "<<endl;
-     for(i=0;i<3;i++)
-    {
-
-    for(j=0;j<3;j++)
-
-   {
-       cout<<a[i][j]<<" ";
-   }
-   cout<<endl;
-    }
-
-
-    return 0;
+class cylinder{
+public:
+float r,h,pi=3.142;
+float volume(){
+    return (pi*r*r*h);
+}float surface_area;
+float surface_Area(){
+ surface_area=(2*pi*r*r)+(pi*r*h);
+ return surface_area;
 }
+}int main(){
+    cylinde cylinder1;
+    int a,b,volume,area;
+    a=cylinder1.r=4;
+    b=cylinde1.h=10
+
+
+
+
